bd6afd8659ad109880353c2283cc9688bc9c2fd7
mindmantra <themindmantra@gmail.com>
2018-05-05 02:35:17 +0530

    commented out unneccessary images from update script

--- update-docker.sh
+++ update-docker.sh
@@ -46,7 +46,7 @@ docker pull cytopia/mysql-5.5:${TAG}
 docker pull cytopia/mysql-5.6:${TAG}
 docker pull cytopia/mysql-5.7:${TAG}
 docker pull cytopia/mysql-8.0:${TAG}
-docker pull cytopia/mariadb-5.5:${TAG}
+#docker pull cytopia/mariadb-5.5:${TAG}
 docker pull cytopia/mariadb-10.0:${TAG}
 docker pull cytopia/mariadb-10.1:${TAG}
 docker pull cytopia/mariadb-10.2:${TAG}
@@ -55,63 +55,63 @@ docker pull cytopia/mariadb-10.3:${TAG}
 ###
 ### PGSQL
 ###
-docker pull postgres:9.1
-docker pull postgres:9.2
-docker pull postgres:9.3
-docker pull postgres:9.4
-docker pull postgres:9.5
-docker pull postgres:9.6
-docker pull postgres:10.0
-docker pull postgres:10.1
-docker pull postgres:10.2
+#docker pull postgres:9.1
+#docker pull postgres:9.2
+#docker pull postgres:9.3
+#docker pull postgres:9.4
+#docker pull postgres:9.5
+#docker pull postgres:9.6
+#docker pull postgres:10.0
+#docker pull postgres:10.1
+#docker pull postgres:10.2
 docker pull postgres:10.3
 
 ###
 ### REDIS
 ###
-docker pull redis:2.8
-docker pull redis:3.0
-docker pull redis:3.2
+#docker pull redis:2.8
+#docker pull redis:3.0
+#docker pull redis:3.2
 docker pull redis:4.0
 
 ###
 ### MEMCACHED
 ###
-docker pull memcached:1.4.21
-docker pull memcached:1.4.22
-docker pull memcached:1.4.23
-docker pull memcached:1.4.24
-docker pull memcached:1.4.25
-docker pull memcached:1.4.26
-docker pull memcached:1.4.27
-docker pull memcached:1.4.28
-docker pull memcached:1.4.29
-docker pull memcached:1.4.30
-docker pull memcached:1.4.31
-docker pull memcached:1.4.32
-docker pull memcached:1.4.33
-docker pull memcached:1.4.34
-docker pull memcached:1.4.35
-docker pull memcached:1.4.36
-docker pull memcached:1.4.37
-docker pull memcached:1.4.38
-docker pull memcached:1.4.39
-docker pull memcached:1.5.0
-docker pull memcached:1.5.1
-docker pull memcached:1.5.2
-docker pull memcached:1.5.3
+#docker pull memcached:1.4.21
+#docker pull memcached:1.4.22
+#docker pull memcached:1.4.23
+#docker pull memcached:1.4.24
+#docker pull memcached:1.4.25
+#docker pull memcached:1.4.26
+#docker pull memcached:1.4.27
+#docker pull memcached:1.4.28
+#docker pull memcached:1.4.29
+#docker pull memcached:1.4.30
+#docker pull memcached:1.4.31
+#docker pull memcached:1.4.32
+#docker pull memcached:1.4.33
+#docker pull memcached:1.4.34
+#docker pull memcached:1.4.35
+#docker pull memcached:1.4.36
+#docker pull memcached:1.4.37
+#docker pull memcached:1.4.38
+#docker pull memcached:1.4.39
+#docker pull memcached:1.5.0
+#docker pull memcached:1.5.1
+#docker pull memcached:1.5.2
+#docker pull memcached:1.5.3
 docker pull memcached:1.5.4
-docker pull memcached:1.5.5
-docker pull memcached:latest
+#docker pull memcached:1.5.5
+#docker pull memcached:latest
 
 ###
 ### MONGODB
 ###
-docker pull mongo:2.8
-docker pull mongo:3.0
-docker pull mongo:3.2
-docker pull mongo:3.4
-docker pull mongo:3.5
-docker pull mongo:3.6
-docker pull mongo:3.7
+#docker pull mongo:2.8
+#docker pull mongo:3.0
+#docker pull mongo:3.2
+#docker pull mongo:3.4
+#docker pull mongo:3.5
+#docker pull mongo:3.6
+#docker pull mongo:3.7
 
